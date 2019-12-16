60cc3736aed31e221ee4bb8996d81301fcabeaf1
Chihyu <chihyu10@gmail.com>
2019-12-16 12:21:19 +0100

    Fourth exercise done

--- 04-asv-confirm/script.js
+++ 04-asv-confirm/script.js
@@ -10,7 +10,20 @@
 // You will have time to focus on it later.
 
 (function() {
+    // var name = prompt("What is your name");
+    // var Age = prompt("How old are you");
+    // // var Town= prompt("In what city do you live?");
+    //
+    // window.confirm("So your name is" + name + age )
 
-    // your code here
+    var name = prompt('What is your name');
+    var age = prompt('How old are you');
+    var city = prompt('Where do you live?');
+    var promptconfirm = window.confirm ('Your name = ' + name + '\nYou are ' + age + '\nYou live in ' + city);
+    if (promptconfirm === false) {
+        location.reload()
 
+    } else {
+        alert("Thank you");
+    }
 })();

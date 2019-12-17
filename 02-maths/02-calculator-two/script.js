88b35c40066c0d9e92e99fab7701dcf417dddcb8
Chihyu <chihyu10@gmail.com>
2019-12-17 12:24:55 +0100

    Edited HTML to work with JS switch

--- 02-maths/02-calculator-two/script.js
+++ 02-maths/02-calculator-two/script.js
@@ -9,10 +9,28 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-function test() {
-    return true;
-}
+function calc(chih) {
+    let opone = document.getElementById("op-one").value;
+    let optwo = document.getElementById("op-two").value;
+    let solution;
 
-(function() {
-    return true;
-})();
+// Don't forget the break! that will cause it to stop the switch (getting out)
+    switch (chih) {
+        case "addition":
+            solution = parseInt(opone) + parseInt(optwo);
+            alert(solution);
+            break;
+        case "substraction":
+            solution = parseInt(opone) - parseInt(optwo);
+            alert(solution);
+            break;
+        case "multiplication":
+            solution = parseInt(opone) * parseInt(optwo);
+            alert(solution);
+            break;
+        case "division":
+            solution = parseInt(opone) / parseInt(optwo);
+            alert(solution);
+    }
+
+}

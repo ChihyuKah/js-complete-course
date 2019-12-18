11b9b059eb0b13a4c8baee67ed7fb08dfc1107e1
Chihyu <chihyu10@gmail.com>
2019-12-18 14:49:20 +0100

    Finished code

--- 03-colors/01-change-bcg-one/script.js
+++ 03-colors/01-change-bcg-one/script.js
@@ -9,8 +9,23 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+    // calls the color when being clicked on
+    document.getElementById("red").addEventListener("click", function () {
 
-    // your code here
+        document.body.style.backgroundColor = "red";
+    });
 
-})();
+    document.getElementById("yellow").addEventListener("click", function () {
+
+        document.body.style.backgroundColor = "yellow";
+    });
+    document.getElementById("green").addEventListener("click", function () {
+
+        document.body.style.backgroundColor = "green";
+    });
+    document.getElementById("blue").addEventListener("click", function () {
+
+        document.body.style.backgroundColor = "blue";
+    });
+}());

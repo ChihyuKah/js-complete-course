2dbcc2d4f573a1e4f2b2a682f84231d70a08caeb
Chihyu <chihyu10@gmail.com>
2019-12-19 16:48:11 +0100

    Still trying to figuring it out.

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -1,32 +1,28 @@
-/* becode/javascript
- *
- * /04-dates/04-locale-date/script.js - 4.4: date textuelle
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
     // The format it's asking is DAY, DATE, MONTH , YEAR, HOURh MINUTES
                                 //Sunday 28 December 2018 22h23
     //Judging by the links I searched for I will need to use an Array for this
-    var newDate = new Date();
-    document.getElementById("target").innerHTML = newDate;
+    var newDate ;
 
-    var day = getDay();
-    var date = getDate();
-    var month = getMonth();
-    var year = getYear();
-    var hour = getHour();
-    var minute = getMinute();
+        newDate = new Date();
+        newDate = new Date(2151153156);
+        newDate = ("25-01-2018T23:30");
+
+        console.log(newDate.toString());
+    document.getElementById("target").innerHTML = newDate;
+    // Problem was not getting assigned
+    // var day = newDate.getDay();
+    // var date = newDate.getDate();
+    // var month = newDate.getMonth();
+    // var year = newDate.getFullYear();
+    // var hour = newDate.getHours();
+    // var minute = newDate.getMinutes();
 
         //testing purposes
 
-    console.log("day" + " " + "date" + " " + "month" + " " + "year" + " " + "hour" + " " + "minute")
+    console.log("dah" + " " + "date" + " " + "month" + " " + "year" + " " + "hour" + " " + "minute")
 
-   
+
 
 })();

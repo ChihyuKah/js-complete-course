129164336f625257df585389b9d2a660333cef28
Chihyu <chihyu10@gmail.com>
2019-12-19 17:01:07 +0100

    Adapting the codes by using the tut

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -3,13 +3,12 @@
     // The format it's asking is DAY, DATE, MONTH , YEAR, HOURh MINUTES
                                 //Sunday 28 December 2018 22h23
     //Judging by the links I searched for I will need to use an Array for this
-    var newDate ;
+    var dob = new Date();
+    var day = dob.getDay();
 
-        newDate = new Date();
-        newDate = new Date(2151153156);
-        newDate = ("25-01-2018T23:30");
+    console.log(day);
+    
 
-        console.log(newDate.toString());
     document.getElementById("target").innerHTML = newDate;
     // Problem was not getting assigned
     // var day = newDate.getDay();

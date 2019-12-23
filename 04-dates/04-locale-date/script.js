9305ba61c6ecfc55f4607bcb7407f1edf10652d1
Chihyu <chihyu10@gmail.com>
2019-12-23 10:11:15 +0100

    Finished code

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -3,32 +3,27 @@
     // The format it's asking is DAY, DATE, MONTH , YEAR, HOURh MINUTES
                                 //Sunday 28 December 2018 22h23
     //Judging by the links I searched for I will need to use an Array for this
-    var dob = new Date();
-    var day = dob.getDay();
-
-    console.log(day);
-
-    document.getElementById("target").innerHTML = newDate;
-    //Problem was not getting assigned
-    var day = newDate.getDay();
-    var date = newDate.getDate();
-    var month = newDate.getMonth();
-    var year = newDate.getFullYear();
-    var hour = newDate.getHours();
-    var minute = newDate.getMinutes();
-
-        //testing purposes
 
 
-        let days=["Sunday", "Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
-        let months=["January","February","March","April","May","June","July","August","September","October","November","December"];
+        var newDate = new Date();
 
-            var DayOfWeek ="";
-            var fullMonth ="";
+        var day = newDate.getDay();
+        var date = newDate.getDate();
+        var month = newDate.getMonth();
+        var year = newDate.getFullYear();
+        var hour = newDate.getHours();
+        var minute = newDate.getMinutes();
 
-            DayOfWeek = days [day];
-            fullMonth = months [month];
+            const days=["Sunday", "Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
+            const months=["January","February","March","April","May","June","July","August","September","October","November","December"];
 
-    document.getElementById("target").innerHTML = weekDay +" "+ date +" " + fullMonth + " " + year +"," + hour +"h" + min;
+            var dayOfWeek ="";
+            var fullMonths="";
 
+            dayOfWeek = days[day];
+            fullMonths = months[month];
+
+
+            document.getElementById("target").innerHTML = dayOfWeek +" "+ date +" " + fullMonths + " " + year +"," + hour +"h" + minute;
+       
 })();

0b374ee924f5fc12b45d989c9ae35104eec71576
Chihyu <chihyu10@gmail.com>
2019-12-23 14:56:08 +0100

    Finally some working code

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -9,35 +9,73 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+// (function() {
+//     // when you click
+//     document.getElementById("run").addEventListener("click", function(){
+//
+//         // get the value
+//         var worth = document.getElementById("year").value;
+//
+//
+//
+//         //get dates
+//         var d= new Date();
+//
+//         d.setFullYear(worth, 0,13);
+//
+//
+//         var months=[];
+//         const mon =["January","February","March","April","May","June","July","August","September","October","November","December"];
+//
+//         for (var m=0; m<=11;m++){
+//
+//             d.setMonth(m);
+//
+//             // m=2 -> d = 2000 2 13
+//
+//             if ( d.getDay() === 5){
+//                 months.push(mon[m])
+//             }
+//         }
+//
+//         // show
+//
+//         confirm(months);
+//
+//
+//
+//
+//     })
+//
+// })();
+
 (function() {
     document.getElementById("run").addEventListener("click", function () {
 
-        var test = document.getElementById("year").value;
+        var value = document.getElementById("year").value;
 
         //To get a new date that starts with the 13th
         var d = new Date();
-        d.setFullYear(year , 0, 13);
+        d.setFullYear(value, 0, 13);
 
-
-        var months =[];
-        var m = ["January" + "" + "February" + "" + "March" + "" + "April" + "" + "June" + "" + "July" + "" +
-        "August" + "" + "September" + "" + "October" + "" + "November" + "" + "December"];
-
+        //It does not need the +
+        var months = [];
+        var mon = ["January","February","March","April","June","July", "August","September","October","November","December"];
 
 
 
-        for (m=0; m <= 11; m++)
-        // while(months <= 11) {
-        //     months++;
-        //     if(d.getDay() == 5 && d.getDate() == 13) {
-        //         counter++;
-        //     }
-        // }
-        console.log(counter);
+        for (var m = 0; m <= 11; m++) {
+//
+            d.setMonth(m);
 
-    })
-    // your code here
+            if (d.getDay() === 5) {
+                months.push(mon[m]);
 
+                confirm(months);
+
+            }
+        }
+
+    });
 })();
-
 

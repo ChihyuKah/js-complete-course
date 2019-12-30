d46da85c8508b78f610f589db0f7bd527c73e7ec
Chihyu <chihyu10@gmail.com>
2019-12-30 10:33:48 +0100

    Working on first part of the code

---
+++ 05-arrays/09-rand-array-stats/script.js
@@ -0,0 +1,23 @@
+/* becode/javascript
+ *
+ * /05-arrays/09-rand-array-stats/script.js - 5.9: tableau aléatoire & statistiques
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+   document.getElementById("run").addEventListener("click", function () {
+
+       //getting random number
+      var test = math.floor(math.random() * (max - min + 1)) + min;
+   });
+        console.log("test");
+
+    var newNumber = [9];
+})();
+

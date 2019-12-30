6cc35e68defdb319806ce22dd840e09257438075
Chihyu <chihyu10@gmail.com>
2019-12-30 12:17:22 +0100

    Understood the logic behind the code

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -12,12 +12,59 @@
 (function() {
 
    document.getElementById("run").addEventListener("click", function () {
+        // giving each id a random number
+      var randomNr1 =  Math.floor(Math.random() * (100));
+      var randomNr2 =  Math.floor(Math.random() * (100));
+      var randomNr3 =  Math.floor(Math.random() * (100));
+      var randomNr4 =  Math.floor(Math.random() * (100));
+      var randomNr5 =  Math.floor(Math.random() * (100));
+      var randomNr6 =  Math.floor(Math.random() * (100));
+      var randomNr7 =  Math.floor(Math.random() * (100));
+      var randomNr8 =  Math.floor(Math.random() * (100));
+      var randomNr9 =  Math.floor(Math.random() * (100));
+      var randomNr10 =  Math.floor(Math.random() * (100));
 
-       //getting random number
-      var test = math.floor(math.random() * (max - min + 1)) + min;
-   });
-        console.log("test");
 
-    var newNumber = [9];
+        //Post it to the HTML
+      document.getElementById("n-1").innerHTML = randomNr1;
+      document.getElementById("n-2").innerHTML = randomNr2;
+      document.getElementById("n-3").innerHTML = randomNr3;
+      document.getElementById("n-4").innerHTML = randomNr4;
+      document.getElementById("n-5").innerHTML = randomNr5;
+      document.getElementById("n-6").innerHTML = randomNr6;
+      document.getElementById("n-7").innerHTML = randomNr7;
+      document.getElementById("n-8").innerHTML = randomNr8;
+      document.getElementById("n-9").innerHTML = randomNr9;
+      document.getElementById("n-10").innerHTML = randomNr10;
+
+        // All the numbers combined in an array
+
+       var allNumb = [randomNr1, randomNr2, randomNr3, randomNr4, randomNr5, randomNr6, randomNr7, randomNr8, randomNr9, randomNr10];
+
+       //getting min and max value of the given numbers
+      var min = Math.min(...allNumb);
+      var max = Math.max(...allNumb);
+
+      document.getElementById("min").innerHTML = min;
+       document.getElementById("max").innerHTML = max;
+      console.log(min);
+
+
+      //Sum of all numbers given
+       var sum = allNumb.reduce(function(a, b){
+           return a + b;
+       }, 0);
+       document.getElementById("sum").innerHTML = sum;
+
+       var avg = allNumb / allNumb.length;
+
+       document.getElementById("average").innerHTML = avg;
+
+
+
+
+});
+
+
 })();
 

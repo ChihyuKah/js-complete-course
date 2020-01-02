6af66affa82d7cefe4b3c5a7df4b66f204ee0e93
Chihyu <chihyu10@gmail.com>
2020-01-02 14:35:37 +0100

    Finished code

--- 06-dom/09-match-password-one/script.js
+++ 06-dom/09-match-password-one/script.js
@@ -11,6 +11,36 @@
 
 (function() {
 
-    // your code here
+    //on click
+    document.getElementById("run").addEventListener("click", function f() {
+
+        //to get the values of both passwords input fields
+        var p1 = document.getElementById("pass-one").value;
+        var p2 = document.getElementById("pass-two").value;
+
+
+        //to test
+        console.log (p1,p2)
+
+        if (p1 === p2) {
+
+            //selecting all the input fields and changing the color of it
+            var x = document.querySelectorAll("input");
+            var i;
+            for (i = 0; i < x.length; i++) {
+                x[i].style.backgroundColor = "green";
+            }
+        } else if (p1 !== p2 ) {
+            var x = document.querySelectorAll("input");
+            var i;
+            for (i = 0; i < x.length; i++) {
+                x[i].style.backgroundColor = "red";
+
+            }
+        }
+
+
+    })
+
 
 })();

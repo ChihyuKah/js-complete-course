369c3dcbccb6ec24fff26b5d548eb25c336e375c
Chihyu <chihyu10@gmail.com>
2020-01-03 15:05:59 +0100

    First part done

--- 06-dom/12-change-event-input-two/script.js
+++ 06-dom/12-change-event-input-two/script.js
@@ -11,6 +11,36 @@
 
 (function() {
 
-    // your code here
+    var input = document.getElementById("pass-one");
+    var val = document.getElementById("validity");
+    document.getElementById('pass-one').setAttribute("minlength",8);
+
+
+    input.addEventListener("input",function () {
+
+        if (input.value.length >= 8 ) {
+
+            val.innerHTML = "Ok";
+        } else if (input.value.length <= 9 ) {
+
+            val.innerHTML = "Not Ok";
+        }
+
+        // val.innerHTML = input.value.length + "/10"
+    })
+
+
+    // for (let i = 0; i < 8;) {
+    //     if (value[i]>= value )
+    // }
+    // if (var input = 0 ; input <= 8;) {
+    //     counter.innerHTML = "Not Ok"
+    // } else if (var input = 0 ; i >= 8; )
+    // for (var i = 0 ; i <= 8 ; ) {
+    //
+    //
+    //     counter.innerHTML = "Not Ok"
+    // }
+
 
 })();
